ced16b6fac8b1c59657522174ef58126044c13ab
Lukáš Peťko <luke.petko@gmail.com>
2024-02-19 22:29:29 +0100

    adding dark mode

--- src/components/text-container/TextContainer.tsx
+++ src/components/text-container/TextContainer.tsx
@@ -39,16 +39,16 @@ const TextContainer = () => {
   return (
     <div
       ref={outerDiv}
-      className="leading-tight h-screen transition-all duration-300 font-display uppercase flex flex-wrap content-start"
+      className="leading-tight h-screen transition-all duration-700 ease-in-out font-display uppercase flex flex-wrap content-start"
     >
       <h1 className="text-[2em] font-bold leading-none">Lukáš Peťko</h1>
-      <h2 className="text-[1.3em] font-semibold">Frontend Developer</h2>
-      <p className="text-[1em] font-thin flex flex-wrap gap-x-16">
-        <span className="first-letter:font-medium">About me</span>
-        <span className="first-letter:font-medium">Projects</span>
-        <span className="first-letter:font-medium">Experience</span>
-        <span className="first-letter:font-medium">Blog</span>
-        <span className="first-letter:font-medium">Contact</span>
+      <h2 className="text-[.75em] font-thin">Frontend Developer</h2>
+      <p className="text-[1.25em] font-regular flex flex-wrap gap-x-10">
+        <span className="first-letter:font-bold">About me</span>
+        <span className="first-letter:font-bold">Projects</span>
+        {/*<span className="first-letter:font-bold">Experience</span>*/}
+        <span className="first-letter:font-bold">Blog</span>
+        <span className="first-letter:font-bold">Contact</span>
       </p>
     </div>
   );
