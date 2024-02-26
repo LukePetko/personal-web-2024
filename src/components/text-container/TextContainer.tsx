eefcb89e9f9d9655240562334c7ffc5ad0979bda
Lukáš Peťko <luke.petko@gmail.com>
2024-02-26 22:45:46 +0100

    fixing non-existing import

--- src/components/text-container/TextContainer.tsx
+++ src/components/text-container/TextContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, type ReactNode } from "react";
+import { useEffect, useRef, useState, type ReactNode } from "react";
 import useFontResize from "./hooks/useFontResize";
 import Cursor from "./Cursor";
 import Link from "./Link";
